7aaef9117d381c3e587688b734da55e7faa15e65
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-10-07 16:31:27 +0300

    Updates

---
+++ precache-manifest.76cc29576955a654aeb3127581dafd11.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/nhl/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "6f15abc9d0fc030c58f0",
+    "url": "/nhl/static/css/main.4c8a7619.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/nhl/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "8a3926069d84b18c3759",
+    "url": "/nhl/static/js/1.8a392606.chunk.js"
+  },
+  {
+    "revision": "bd85c5b513430a366a57",
+    "url": "/nhl/static/js/runtime~main.bd85c5b5.js"
+  },
+  {
+    "revision": "6f15abc9d0fc030c58f0",
+    "url": "/nhl/static/js/main.6f15abc9.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/nhl/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/nhl/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/nhl/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "8a3926069d84b18c3759",
+    "url": "/nhl/static/css/1.01c8fe88.chunk.css"
+  },
+  {
+    "revision": "745a74294d9b4cc6ae99a2c6c0b9e29b",
+    "url": "/nhl/index.html"
+  }
+];
