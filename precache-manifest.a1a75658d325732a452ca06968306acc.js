62bd27c6a4e8b8bf6b259e7cc4447be45bc8493e
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2019-10-09 22:10:08 +0300

    Updates

---
+++ precache-manifest.a1a75658d325732a452ca06968306acc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f5f124afbe67a5c7132ed7b110574b",
+    "url": "/nhl/index.html"
+  },
+  {
+    "revision": "ec40001d529de90cbe75",
+    "url": "/nhl/static/css/2.1dd2dd61.chunk.css"
+  },
+  {
+    "revision": "b6ac05bba9b8c928b257",
+    "url": "/nhl/static/css/main.fc133c46.chunk.css"
+  },
+  {
+    "revision": "ec40001d529de90cbe75",
+    "url": "/nhl/static/js/2.14eda2e1.chunk.js"
+  },
+  {
+    "revision": "b6ac05bba9b8c928b257",
+    "url": "/nhl/static/js/main.d9b6ec5a.chunk.js"
+  },
+  {
+    "revision": "0ef7b655417f9cd94b8a",
+    "url": "/nhl/static/js/runtime-main.ab272387.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/nhl/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/nhl/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/nhl/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/nhl/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/nhl/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nhl/precache-manifest.78794a35b12f8e6779d48784b5136b13.js"
+  "/nhl/precache-manifest.a1a75658d325732a452ca06968306acc.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nhl/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nhl/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
